80f7676a6c82035753ca817a8292c976b524c5ea
amilcar-laniakea <amilcar.laniakea@gmail.com>
2019-11-15 05:22:14 -0400

    Rama para la funcionalidad de Hapi-Joi y Validaciones,product.schema.ts marca error

--- src/product/schemas/product.schema.ts
+++ src/product/schemas/product.schema.ts
@@ -1,15 +1,30 @@
 import { Schema } from "mongoose";
+import { Joi } from "@hapi/joi";
 //formato usado por la base de datos para le majeto de datos
-export const ProductSchema = new Schema({
-    rut: { type: Number, required: true},
-    name: { type: String, required: true},
-    last_name: { type: String, required: true},
-    phone: { type: String, required: true},
-    sex: { type: String, required: false},
-    email: { type: String, required: true},
-    address: {type: String, required: false},
+ const userSchema = new Schema({
+    rut: Number, 
+    name: String, 
+    last_name:String,
+    phone: String,
+    sex: String,
+    email: String,
+    address: String,
     createdAt: {
         type: Date,
         default: Date.now
     }
 });
+export const ProductSchema = userSchema.methods.joiValidate = function(obj) {
+	
+	var schema = {
+        rut: Joi.number().min(9).max(9).required(),
+		name: Joi.string().min(2).max(30).required(),
+        last_name: Joi.string().min(2).max(30).required(),
+        phone: Joi.number().min(11).max(11).required(),
+        sex: Joi.string().min(1).max(1).required(),
+		email: Joi.string().email().required(),
+		address: Joi.types.String().required(),
+		createdAt: Joi.types.Date(),
+	}
+    return Joi.validate(obj, schema);
+}
