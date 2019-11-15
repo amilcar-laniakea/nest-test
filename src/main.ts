be98ed06d26b488098810da08561fe4ea80b0458
amilcar-laniakea <amilcar.laniakea@gmail.com>
2019-11-15 02:36:52 -0400

    Cambios v2

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
+  const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-   //Puerto predeterminado, se cambio a la sentencia de abajo para probar con Heroku
-  await app.listen(process.env.PORT || 3000);
+  //await app.listen(3000); //Puerto predeterminado, se cambio a la sentencia de abajo para probar con Heroku
+  await app.listen(port);
 }
 bootstrap();
